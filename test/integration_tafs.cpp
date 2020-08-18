f23791433e066405dd42a0a80d6f14e289c5a186
Nick Naumenko <nikolai.naumenko@gmail.com>
2020-08-18 23:31:57 +0300

    Added integration tests for Wind Shear and Wind Shear Conditions in TAF reports
    This concludes integration tests for TAF report handling.

--- test/integration_tafs.cpp
+++ test/integration_tafs.cpp
@@ -1543,7 +1543,7 @@ TEST(IntegrationTafs, tempForecastMultipleTn) {
     static const auto rawReport =
         "TAF LSZH 100225Z 1003/1109 VRB02KT CAVOK"
         " TX31/1015Z TN14/1005Z TN16/1105Z"
-        " PROB30 TEMPO 1014/1020 9999 FEW080CB="; // 10 AUG 2020
+        " PROB30 TEMPO 1014/1020 9999 FEW080CB=";  // 10 AUG 2020
     const auto result = metafsimple::simplify(rawReport);
 
     Report refReport;
@@ -1601,6 +1601,492 @@ TEST(IntegrationTafs, tempForecastMultipleTn) {
     refForecast.minTemperature.push_back(TemperatureForecast{
         Temperature{16, Temperature::Unit::C},
         Time{11, 5, 0}});
+    EXPECT_EQ(result.forecast, refForecast);
+
+    EXPECT_EQ(result.aerodrome, Aerodrome());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.current, Current());
+}
+
+TEST(IntegrationTafs, windShearInPrevailing) {
+    static const auto rawReport =
+        "TAF KSTJ 101120Z 1012/1112 19008KT P6SM FEW150 WS020/22040KT"
+        " FM101300 20010KT P6SM BKN150"
+        " FM101900 21007KT P6SM VCTS SCT040CB BKN250"
+        " FM102200 07006KT P6SM VCTS SCT040CB BKN250"
+        " FM110400 09009KT P6SM BKN250=";  //10 AUG 2020
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::TAF;
+    refReport.reportTime = Time{10, 11, 20};
+    refReport.applicableFrom = Time{10, 12, 0};
+    refReport.applicableUntil = Time{11, 12, 0};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "KSTJ";
+    EXPECT_EQ(result.station, refStation);
+
+    Forecast refForecast;
+    // 1012/1112 19008KT P6SM FEW150 WS020/22040KT
+    refForecast.prevailing.windDirectionDegrees = 190;
+    refForecast.prevailing.windSpeed = Speed{8, Speed::Unit::KT};
+    refForecast.prevailing.visibility =
+        Distance{
+            Distance::Details::MORE_THAN,
+            6,
+            Distance::Unit::STATUTE_MILES};
+    refForecast.prevailing.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.prevailing.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::FEW,
+                   Height{15000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.prevailing.windShear.push_back(
+        WindShear{
+            Height{2000, Height::Unit::FEET},
+            220,
+            Speed{40, Speed::Unit::KT}});
+
+    // FM101300 20010KT P6SM BKN150
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{10, 13, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 200;
+    refForecast.trends.back().forecast.windSpeed = Speed{10, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::BROKEN,
+                   Height{15000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+
+    // FM101900 21007KT P6SM VCTS SCT040CB BKN250
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{10, 19, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 210;
+    refForecast.trends.back().forecast.windSpeed = Speed{7, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::SCATTERED,
+                   Height{4000, Height::Unit::FEET},
+                   CloudLayer::Details::CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::BROKEN,
+                   Height{25000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.trends.back().vicinity.insert(ObservedPhenomena::THUNDERSTORM);
+
+    // FM102200 07006KT P6SM VCTS SCT040CB BKN250
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{10, 22, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 70;
+    refForecast.trends.back().forecast.windSpeed = Speed{6, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::SCATTERED,
+                   Height{4000, Height::Unit::FEET},
+                   CloudLayer::Details::CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::BROKEN,
+                   Height{25000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.trends.back().vicinity.insert(ObservedPhenomena::THUNDERSTORM);
+
+    // FM110400 09009KT P6SM BKN250
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{11, 4, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 90;
+    refForecast.trends.back().forecast.windSpeed = Speed{9, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::BROKEN,
+                   Height{25000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    EXPECT_EQ(result.forecast, refForecast);
+
+    EXPECT_EQ(result.aerodrome, Aerodrome());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.current, Current());
+}
+
+TEST(IntegrationTafs, windShearInTrend) {
+    static const auto rawReport =
+        "TAF KVTN 131737Z 1318/1418 21007KT P6SM SKC"
+        " FM132000 28007KT P6SM SKC"
+        " FM132100 31006KT P6SM SKC"
+        " FM132300 36007KT P6SM FEW200"
+        " FM141100 20011KT P6SM SCT035 SCT200 WS020/22040KT"
+        " FM141300 22012KT P6SM FEW035="; // 13 AUG 2020
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::TAF;
+    refReport.reportTime = Time{13, 17, 37};
+    refReport.applicableFrom = Time{13, 18, 0};
+    refReport.applicableUntil = Time{14, 18, 0};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "KVTN";
+    EXPECT_EQ(result.station, refStation);
+
+    Forecast refForecast;
+    // 1318/1418 21007KT P6SM SKC
+    refForecast.prevailing.windDirectionDegrees = 210;
+    refForecast.prevailing.windSpeed = Speed{7, Speed::Unit::KT};
+    refForecast.prevailing.visibility =
+        Distance{
+            Distance::Details::MORE_THAN,
+            6,
+            Distance::Unit::STATUTE_MILES};
+    refForecast.prevailing.skyCondition =
+        Essentials::SkyCondition::CLEAR_SKC;
+
+    // FM132000 28007KT P6SM SKC
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{13, 20, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 280;
+    refForecast.trends.back().forecast.windSpeed = Speed{7, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLEAR_SKC;
+
+    // FM132100 31006KT P6SM SKC
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{13, 21, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 310;
+    refForecast.trends.back().forecast.windSpeed = Speed{6, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLEAR_SKC;
+
+    // FM132300 36007KT P6SM FEW200
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{13, 23, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 360;
+    refForecast.trends.back().forecast.windSpeed = Speed{7, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::FEW,
+                   Height{20000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+
+    // FM141100 20011KT P6SM SCT035 SCT200 WS020/22040KT
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{14, 11, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 200;
+    refForecast.trends.back().forecast.windSpeed = Speed{11, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::SCATTERED,
+                   Height{3500, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::SCATTERED,
+                   Height{20000, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    refForecast.trends.back().forecast.windShear.push_back(
+        WindShear{
+            Height{2000, Height::Unit::FEET},
+            220,
+            Speed{40, Speed::Unit::KT}});
+
+    // FM141300 22012KT P6SM FEW035
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{14, 13, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 220;
+    refForecast.trends.back().forecast.windSpeed = Speed{12, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN,
+                 6,
+                 Distance::Unit::STATUTE_MILES};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLOUDS;
+    refForecast.trends.back().forecast.cloudLayers.push_back(
+        CloudLayer{CloudLayer::Amount::FEW,
+                   Height{3500, Height::Unit::FEET},
+                   CloudLayer::Details::NOT_TOWERING_CUMULUS_NOT_CUMULONIMBUS,
+                   std::optional<int>()});
+    EXPECT_EQ(result.forecast, refForecast);
+
+    EXPECT_EQ(result.aerodrome, Aerodrome());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.current, Current());
+}
+
+TEST(IntegrationTafs, wscondsInTrend) {
+    static const auto rawReport =
+        "TAF KNJK 0615/0715 25020G25KT 9999 SKC 520009 QNH2963INS"
+        " TEMPO 0616/0618 25015KT"
+        " FM070500 25008KT 9999 SKC WSCONDS 530009 QNH2970INS"
+        " T40/0622Z T25/0713Z"
+        " FS30130=";  //6 AUG 2020
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::TAF;
+    refReport.reportTime = Time();
+    refReport.applicableFrom = Time{6, 15, 0};
+    refReport.applicableUntil = Time{7, 15, 0};
+    refReport.error = Report::Error::NO_ERROR;
+    refReport.plainText.push_back("FS30130");
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "KNJK";
+    EXPECT_EQ(result.station, refStation);
+
+    Forecast refForecast;
+    // 0615/0715 25020G25KT 9999 SKC 520009 QNH2963INS
+    refForecast.prevailing.windDirectionDegrees = 250;
+    refForecast.prevailing.windSpeed = Speed{20, Speed::Unit::KT};
+    refForecast.prevailing.gustSpeed = Speed{25, Speed::Unit::KT};
+    refForecast.prevailing.visibility =
+        Distance{
+            Distance::Details::MORE_THAN,
+            10000,
+            Distance::Unit::METERS};
+    refForecast.prevailing.skyCondition =
+        Essentials::SkyCondition::CLEAR_SKC;
+    refForecast.prevailingTurbulence.push_back(
+        TurbulenceForecast{
+            TurbulenceForecast::Severity::MODERATE,
+            TurbulenceForecast::Location::IN_CLEAR_AIR,
+            TurbulenceForecast::Frequency::OCCASIONAL,
+            Height{0, Height::Unit::FEET},
+            Height{9000, Height::Unit::FEET},
+        });
+    refForecast.prevailing.seaLevelPressure =
+        Pressure{2963, Pressure::Unit::HUNDREDTHS_IN_HG};
+
+    // TEMPO 0616/0618 25015KT
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TEMPO,
+            std::optional<int>(),
+            Time{6, 16, 0},
+            Time{6, 18, 0},
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 250;
+    refForecast.trends.back().forecast.windSpeed = Speed{15, Speed::Unit::KT};
+
+    // FM070500 25008KT 9999 SKC WSCONDS 530009 QNH2970INS
+    refForecast.trends.push_back(
+        Trend{
+            Trend::Type::TIMED,
+            std::optional<int>(),
+            Time{7, 5, 0},
+            Time(),
+            Time(),
+            Essentials(),
+            {},
+            {},
+            {},
+        });
+    refForecast.trends.back().forecast.windDirectionDegrees = 250;
+    refForecast.trends.back().forecast.windSpeed = Speed{8, Speed::Unit::KT};
+    refForecast.trends.back().forecast.visibility =
+        Distance{Distance::Details::MORE_THAN, 10000, Distance::Unit::METERS};
+    refForecast.trends.back().forecast.skyCondition =
+        Essentials::SkyCondition::CLEAR_SKC;
+    refForecast.trends.back().windShearConditions = true;
+    refForecast.trends.back().turbulence.push_back(
+        TurbulenceForecast{
+            TurbulenceForecast::Severity::MODERATE,
+            TurbulenceForecast::Location::IN_CLEAR_AIR,
+            TurbulenceForecast::Frequency::FREQUENT,
+            Height{0, Height::Unit::FEET},
+            Height{9000, Height::Unit::FEET},
+        });
+    refForecast.trends.back().forecast.seaLevelPressure = 
+        Pressure{2970, Pressure::Unit::HUNDREDTHS_IN_HG};
+
+    // T40/0622Z T25/0713Z
+    refForecast.maxTemperature.push_back(TemperatureForecast{
+        Temperature{40, Temperature::Unit::C},
+        Time{6, 22, 0}});
+    refForecast.minTemperature.push_back(TemperatureForecast{
+        Temperature{25, Temperature::Unit::C},
+        Time{7, 13, 0}});
+    EXPECT_EQ(result.forecast, refForecast);
+
+    EXPECT_EQ(result.aerodrome, Aerodrome());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.current, Current());
+}
+
+TEST(IntegrationTafs, wscondsInPrevailing) {
+    static const auto rawReport =
+        "TAF ZZZZ 1823/1923 25020KT 9999 SKC WSCONDS=";
+        // Fake report created for this test
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::TAF;
+    refReport.reportTime = Time();
+    refReport.applicableFrom = Time{18, 23, 0};
+    refReport.applicableUntil = Time{19, 23, 0};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "ZZZZ";
+    EXPECT_EQ(result.station, refStation);
+
+    Forecast refForecast;
+    // 1823/1923 25020KT 9999 SKC WSCONDS
+    refForecast.prevailing.windDirectionDegrees = 250;
+    refForecast.prevailing.windSpeed = Speed{20, Speed::Unit::KT};
+    refForecast.prevailing.visibility =
+        Distance{
+            Distance::Details::MORE_THAN,
+            10000,
+            Distance::Unit::METERS};
+    refForecast.prevailing.skyCondition = Essentials::SkyCondition::CLEAR_SKC;
+    refForecast.prevailingWsConds = true;
     EXPECT_EQ(result.forecast, refForecast);
 
     EXPECT_EQ(result.aerodrome, Aerodrome());
