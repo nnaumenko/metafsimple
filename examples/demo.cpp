7be40434ea5c5283e4de5a674cba0e7d08835f08
Nick Naumenko <nikolai.naumenko@gmail.com>
2020-10-14 23:54:46 +0300

    Added station data to Webassembly Demo

--- examples/demo.cpp
+++ examples/demo.cpp
@@ -6,9 +6,9 @@
 * of the MIT license. See the LICENSE file for details.
 */
 
+#include <algorithm>
 #include <iostream>
 #include <sstream>
-#include <algorithm>
 
 #include "metafsimple.hpp"
 
@@ -39,7 +39,7 @@ std::string ordinalNumber(int i) {
     return (std::to_string(i) + suffix(i));
 }
 
-std::string formatReport(const std::string & s) {
+std::string formatReport(const std::string& s) {
     std::string r = s;
     r.erase(std::remove(r.begin(), r.end(), '\n'), r.end());
     r.erase(std::remove(r.begin(), r.end(), '\r'), r.end());
@@ -131,7 +131,7 @@ std::string toStr(const Temperature& t) {
     }
 }
 
-std::string toStr(Report report) {
+std::string toStr(const Report& report) {
     auto type = [](Report::Type t) {
         switch (t) {
             case Report::Type::METAR:
@@ -230,6 +230,226 @@ std::string toStr(Report report) {
     return result.str();
 }
 
+std::string toStr(const Station& station) {
+    auto autoType = [](Station::AutoType at) {
+        switch (at) {
+            case Station::AutoType::NONE:
+                return "";
+            case Station::AutoType::AO1:
+                return "This automated station is not equipped with a "
+                       "precipitation discriminator";
+            case Station::AutoType::AO1A:
+                return "This automated station is not equipped with a "
+                       "precipitation discriminator and observation is "
+                       "augmented by a human observer";
+            case Station::AutoType::AO2:
+                return "This automated station is equipped with a "
+                       "precipitation discriminator";
+            case Station::AutoType::AO2A:
+                return "This automated station is equipped with a "
+                       "precipitation discriminator and observation is "
+                       "augmented by a human observer";
+        }
+    };
+    auto missingData = [](Station::MissingData md) {
+        switch (md) {
+            case Station::MissingData::WND_MISG:
+                return "wind data is missing";
+            case Station::MissingData::VIS_MISG:
+                return "visibility data is missing";
+            case Station::MissingData::RVR_MISG:
+                return "runway visual range data is missing";
+            case Station::MissingData::RVRNO:
+                return "runway visual range should be present"
+                       " but not available";
+            case Station::MissingData::VISNO:
+                return "visibility data not available";
+            case Station::MissingData::VISNO_RUNWAY:
+                return "visibility data not available for at least one runway";
+            case Station::MissingData::VISNO_DIRECTION:
+                return "visibility data not available for at least one "
+                       "cardinal direction";
+            case Station::MissingData::CHINO:
+                return "ceiling data not available";
+            case Station::MissingData::CHINO_RUNWAY:
+                return "ceiling data not available for at least one runway";
+            case Station::MissingData::CHINO_DIRECTION:
+                return "ceiling data not available for at least one "
+                       "cardinal direction";
+            case Station::MissingData::PWINO:
+                return "this automated station is equipped with "
+                       "present weather identifier "
+                       "and this sensor is not operating";
+            case Station::MissingData::TSNO:
+                return "this automated station is equipped with lightning "
+                       "detector and this sensor is not operating";
+            case Station::MissingData::PNO:
+                return "this automated station is equipped with tipping "
+                       "bucket rain gauge and this sensor is not operating";
+            case Station::MissingData::FZRANO:
+                return "this automated station is equipped with freezing rain "
+                       "sensor and this sensor is not operating";
+            case Station::MissingData::SLPNO:
+                return "mean sea-level pressure information is not available";
+            case Station::MissingData::TS_LTNG_TEMPO_UNAVBL:
+                return "thunderstorm and lightning data is missing";
+            case Station::MissingData::CLD_MISG:
+                return "sky condition data is missing";
+            case Station::MissingData::WX_MISG:
+                return "weather phenomena data is missing";
+            case Station::MissingData::T_MISG:
+                return "temperature data is missing";
+            case Station::MissingData::TD_MISG:
+                return "dew point data is missing";
+            case Station::MissingData::PRES_MISG:
+                return "atmospheric pressure data is missing";
+            case Station::MissingData::ICG_MISG:
+                return "icing data is missing";
+            case Station::MissingData::PCPN_MISG:
+                return "precipitation data is missing";
+        }
+    };
+    std::ostringstream result;
+    result << "icaoCode: station ICAO code " << station.icaoCode << newLine;
+    if (station.autoType != Station::AutoType::NONE) {
+        result << "autoType: ";
+        result << autoType(station.autoType) << newLine;
+    }
+    if (station.requiresMaintenance) {
+        result << "requiresMaintenance: ";
+        result << "automated station requires maintenance" << newLine;
+    }
+    if (station.noSpeciReports) {
+        result << "noSpeciReports: this station does not issue SPECI reports";
+        result << newLine;
+    }
+    if (station.noVisDirectionalVariation) {
+        result << "noVisDirectionalVariation: this station cannot ";
+        result << "differentiate the directional variation of visibility";
+        result << newLine;        
+    }
+    if (!station.missingData.empty()) {
+        result << "missingData: station reports that the following data ";
+        result << "are missing" << newLine;
+        for (const auto md : station.missingData) {
+            result << " - " << missingData(md) << newLine;
+        }
+    }
+    if (!station.runwaysNoCeilingData.empty()) {
+        result << "runwaysNoCeilingData: station reports that the ceiling ";
+        result << "data is missing for the following runways" << newLine;
+        for (const auto rw : station.runwaysNoCeilingData) {
+            result << " - runway " << toStr(rw) << newLine;
+        }
+    }
+    if (!station.runwaysNoVisData.empty()) {
+        result << "runwaysNoVisData: station reports that the visibility ";
+        result << "data is missing for the following runways" << newLine;
+        for (const auto rw : station.runwaysNoVisData) {
+            result << " - runway " << toStr(rw) << newLine;
+        }
+    }
+    return result.str();
+}
+
+std::string toStr(const Aerodrome& aerodrome) {
+    auto colourCode = [](Aerodrome::ColourCode c) {
+        switch(c) {
+            case Aerodrome::ColourCode::NOT_SPECIFIED:
+            return "";
+            case Aerodrome::ColourCode::BLUE:
+            return "blue";
+            case Aerodrome::ColourCode::WHITE:
+            return "white";
+            case Aerodrome::ColourCode::GREEN:
+            return "green";
+            case Aerodrome::ColourCode::YELLOW1:
+            return "yellow1";
+            case Aerodrome::ColourCode::YELLOW2:
+            return "yellow2";
+            case Aerodrome::ColourCode::AMBER:
+            return "amber";
+            case Aerodrome::ColourCode::RED:
+            return "red";
+        }
+    };
+    auto rvrTrend = [](Aerodrome::RvrTrend r) {
+        switch(r) {
+            case Aerodrome::RvrTrend::UNKNOWN:
+            return "";
+            case Aerodrome::RvrTrend::DOWNWARD:
+            return "downward";
+            case Aerodrome::RvrTrend::NEUTRAL:
+            return "neutral";
+            case Aerodrome::RvrTrend::UPWARD:
+            return "upward";
+        }
+    };
+    auto runwayDeposits = [](Aerodrome::RunwayDeposits d) {
+        switch(d) {
+            case Aerodrome::RunwayDeposits::UNKNOWN:
+            return "";
+            case Aerodrome::RunwayDeposits::CLEAR_AND_DRY:
+            return "clear and dry";
+            case Aerodrome::RunwayDeposits::DAMP:
+            return "damp";
+            case Aerodrome::RunwayDeposits::WET_AND_WATER_PATCHES:
+            return "wet and there are water patches";
+            case Aerodrome::RunwayDeposits::RIME_AND_FROST_COVERED:
+            return "rime and frost covered";
+            case Aerodrome::RunwayDeposits::DRY_SNOW:
+            return "dry snow covered";
+            case Aerodrome::RunwayDeposits::WET_SNOW:
+            return "wet snow covered";
+            case Aerodrome::RunwayDeposits::SLUSH:
+            return "slush covered";
+            case Aerodrome::RunwayDeposits::ICE:
+            return "ice covered";
+            case Aerodrome::RunwayDeposits::COMPACTED_OR_ROLLED_SNOW:
+            return "covered in compacted or rolled snow";
+            case Aerodrome::RunwayDeposits::FROZEN_RUTS_OR_RIDGES:
+            return "ice or snow covered with frozen ruts or ridges";
+        }
+    };
+    auto runwayContamExtent = [](Aerodrome::RunwayContamExtent r) {
+        switch(r) {
+            case Aerodrome::RunwayContamExtent::UNKNOWN:
+            return "";
+            case Aerodrome::RunwayContamExtent::NO_DEPOSITS:
+            return "none";
+            case Aerodrome::RunwayContamExtent::LESS_THAN_11_PERCENT:
+            return "less than 11 percent";
+            case Aerodrome::RunwayContamExtent::FROM_11_TO_25_PERCENT:
+            return "11 to 25 percent";
+            case Aerodrome::RunwayContamExtent::FROM_26_TO_50_PERCENT:
+            return "26 to 50 percent";
+            case Aerodrome::RunwayContamExtent::MORE_THAN_50_PERCENT:
+            return "more than 50 percent";
+        }
+    };
+    auto brakingAction = [](Aerodrome::BrakingAction ba) {
+        switch(ba) {
+            case Aerodrome::BrakingAction::UNKNOWN:
+            return "";
+            case Aerodrome::BrakingAction::POOR:
+            return "poor";
+            case Aerodrome::BrakingAction::MEDIUM_POOR:
+            return "medium-poor";
+            case Aerodrome::BrakingAction::MEDIUM:
+            return "medium";
+            case Aerodrome::BrakingAction::MEDIUM_GOOD:
+            return "medium-good";
+            case Aerodrome::BrakingAction::GOOD:
+            return "good";
+            case Aerodrome::BrakingAction::UNRELIABLE:
+            return "unreliable or unmeasurable";
+        }
+    };
+    std::ostringstream result;
+    // TODO
+    return result.str();
+}
+
 std::string demo(const std::string& report) {
     const auto simple = simplify(report);
     std::ostringstream result;
@@ -239,10 +459,10 @@ std::string demo(const std::string& report) {
 
     result << "report (report type, time, attributes, parsing info, etc.)\n";
     result << toStr(simple.report) << newPart;
-    /*
+
     result << "station (station name, capabilities, missing data, etc)\n";
     result << toStr(simple.station) << newPart;
-
+/*
     result << "aerodrome (aerodrome, runway, and directional data)\n";
     result << toStr(simple.aerodrome) << newPart;
 
