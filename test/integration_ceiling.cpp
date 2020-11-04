929420409069d025fc142e76c2570811764f08f0
Nick Naumenko <nikolai.naumenko@gmail.com>
2020-11-04 20:52:37 +0200

    Improved test coverage
    Added integration tests for handling ceiling data for runway (CIG 008 RWY27 or CIG 004V006 RWY27) and direction (CIG 008 NW or CIG 008V010 NW).

--- test/integration_ceiling.cpp
+++ test/integration_ceiling.cpp
@@ -14,7 +14,7 @@ using namespace metafsimple;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Tests for ceiling groups (CIG xxx, CIG xxxVxxx and their variants) specified
-// in remarks 
+// in remarks
 ////////////////////////////////////////////////////////////////////////////////
 
 TEST(IntegraionCeiling, ceilingAerodrome) {
@@ -35,11 +35,10 @@ TEST(IntegraionCeiling, ceilingAerodrome) {
     EXPECT_EQ(result.station, refStation);
 
     Aerodrome refAerodrome;
-    refAerodrome.ceiling = Ceiling {
+    refAerodrome.ceiling = Ceiling{
         Height{800, Height::Unit::FEET},
         Height(),
-        Height()  
-    };
+        Height()};
     EXPECT_EQ(result.aerodrome, refAerodrome);
 
     EXPECT_EQ(result.current, Current());
@@ -65,11 +64,136 @@ TEST(IntegraionCeiling, ceilingAerodromeVaraiable) {
     EXPECT_EQ(result.station, refStation);
 
     Aerodrome refAerodrome;
-    refAerodrome.ceiling = Ceiling {
+    refAerodrome.ceiling = Ceiling{
         Height(),
         Height{600, Height::Unit::FEET},
-        Height{800, Height::Unit::FEET}
-    };
+        Height{800, Height::Unit::FEET}};
+    EXPECT_EQ(result.aerodrome, refAerodrome);
+
+    EXPECT_EQ(result.current, Current());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.forecast, Forecast());
+}
+
+TEST(IntegraionCeiling, ceilingRunway) {
+    static const auto rawReport =
+        "METAR ZZZZ 040310Z /////KT //// RMK CIG 008 RWY27C=";
+    //fake report created for this test
+
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::METAR;
+    refReport.reportTime = Time{4, 3, 10};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "ZZZZ";
+    EXPECT_EQ(result.station, refStation);
+
+    Aerodrome refAerodrome;
+    refAerodrome.runways.push_back(Aerodrome::RunwayData());
+    refAerodrome.runways.back().runway = Runway{27, Runway::Designator::CENTER};
+    refAerodrome.runways.back().ceiling = Ceiling{
+        Height{800, Height::Unit::FEET},
+        Height(),
+        Height()};
+    EXPECT_EQ(result.aerodrome, refAerodrome);
+
+    EXPECT_EQ(result.current, Current());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.forecast, Forecast());
+}
+
+TEST(IntegraionCeiling, ceilingRunwayVariable) {
+    static const auto rawReport =
+        "METAR ZZZZ 040310Z /////KT //// RMK CIG 006V008 RWY27C=";
+    //fake report created for this test
+
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::METAR;
+    refReport.reportTime = Time{4, 3, 10};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "ZZZZ";
+    EXPECT_EQ(result.station, refStation);
+
+    Aerodrome refAerodrome;
+    refAerodrome.runways.push_back(Aerodrome::RunwayData());
+    refAerodrome.runways.back().runway = Runway{27, Runway::Designator::CENTER};
+    refAerodrome.runways.back().ceiling = Ceiling{
+        Height(),
+        Height{600, Height::Unit::FEET},
+        Height{800, Height::Unit::FEET}};
+    EXPECT_EQ(result.aerodrome, refAerodrome);
+
+    EXPECT_EQ(result.current, Current());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.forecast, Forecast());
+}
+
+TEST(IntegraionCeiling, ceilingDirection) {
+    static const auto rawReport =
+        "METAR ZZZZ 040310Z /////KT //// RMK CIG 008 NW=";
+    //fake report created for this test
+
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::METAR;
+    refReport.reportTime = Time{4, 3, 10};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "ZZZZ";
+    EXPECT_EQ(result.station, refStation);
+
+    Aerodrome refAerodrome;
+    refAerodrome.directions.push_back(Aerodrome::DirectionData());
+    refAerodrome.directions.back().cardinalDirection = 
+        CardinalDirection::NW;
+    refAerodrome.directions.back().ceiling = Ceiling{
+        Height{800, Height::Unit::FEET},
+        Height(),
+        Height()};
+    EXPECT_EQ(result.aerodrome, refAerodrome);
+
+    EXPECT_EQ(result.current, Current());
+    EXPECT_EQ(result.historical, Historical());
+    EXPECT_EQ(result.forecast, Forecast());
+}
+
+TEST(IntegraionCeiling, ceilingDirectionVariable) {
+    static const auto rawReport =
+        "METAR ZZZZ 040310Z /////KT //// RMK CIG 006V008 NW=";
+    //fake report created for this test
+
+    const auto result = metafsimple::simplify(rawReport);
+
+    Report refReport;
+    refReport.type = Report::Type::METAR;
+    refReport.reportTime = Time{4, 3, 10};
+    refReport.error = Report::Error::NO_ERROR;
+    EXPECT_EQ(result.report, refReport);
+
+    Station refStation;
+    refStation.icaoCode = "ZZZZ";
+    EXPECT_EQ(result.station, refStation);
+
+    Aerodrome refAerodrome;
+    refAerodrome.directions.push_back(Aerodrome::DirectionData());
+    refAerodrome.directions.back().cardinalDirection = 
+        CardinalDirection::NW;
+    refAerodrome.directions.back().ceiling = Ceiling{
+        Height(),
+        Height{600, Height::Unit::FEET},
+        Height{800, Height::Unit::FEET}};
     EXPECT_EQ(result.aerodrome, refAerodrome);
 
     EXPECT_EQ(result.current, Current());
